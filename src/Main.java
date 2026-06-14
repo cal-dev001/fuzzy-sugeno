2e7bd59147083f2dd06ffefce22e02f1196bf36b
cal-dev001 <deevianofcalista@gmail.com>
2026-06-14 13:12:57 +0700

    Implementasi rule fuzzy dan alpha predikat

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,114 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    //TESTING AJA
+    public static void main(String[] args) {
+
+        double kehadiran = 85;
+        double evaluasi = 80;
+        double penelitian = 4;
+        double pengabdian = 3;
+
+        // FUZZIFIKASI
+
+        double kRendah =
+                MembershipFunction.rendah(
+                        kehadiran,
+                        50,
+                        75
+                );
+
+        double kSedang =
+                MembershipFunction.sedang(
+                        kehadiran,
+                        50,
+                        75,
+                        100
+                );
+
+        double kTinggi =
+                MembershipFunction.tinggi(
+                        kehadiran,
+                        75,
+                        100
+                );
+
+        double eRendah =
+                MembershipFunction.rendah(
+                        evaluasi,
+                        50,
+                        75
+                );
+
+        double eSedang =
+                MembershipFunction.sedang(
+                        evaluasi,
+                        50,
+                        75,
+                        100
+                );
+
+        double eTinggi =
+                MembershipFunction.tinggi(
+                        evaluasi,
+                        75,
+                        100
+                );
+
+        double pRendah =
+                MembershipFunction.rendah(
+                        penelitian,
+                        1,
+                        3
+                );
+
+        double pTinggi =
+                MembershipFunction.tinggi(
+                        penelitian,
+                        3,
+                        5
+                );
+
+        double pmRendah =
+                MembershipFunction.rendah(
+                        pengabdian,
+                        1,
+                        4
+                );
+
+        double pmTinggi =
+                MembershipFunction.tinggi(
+                        pengabdian,
+                        2.5,
+                        4
+                );
+
+        // RULE
+
+        double alpha1 = RuleBase.rule1(kRendah);
+        double alpha2 = RuleBase.rule2(eRendah);
+        double alpha3 = RuleBase.rule3(pRendah, pmRendah);
+        double alpha4 = RuleBase.rule4(kSedang, eSedang);
+        double alpha5 = RuleBase.rule5(kTinggi, eSedang);
+        double alpha6 = RuleBase.rule6(kSedang, eTinggi);
+        double alpha7 = RuleBase.rule7(kTinggi, eTinggi);
+        double alpha8 = RuleBase.rule8(eTinggi, pTinggi);
+        double alpha9 = RuleBase.rule9(
+                kTinggi,
+                eTinggi,
+                pTinggi,
+                pmTinggi
+        );
+
+        System.out.println("===== ALPHA PREDIKAT =====");
+
+        System.out.println("Rule 1 = " + alpha1);
+        System.out.println("Rule 2 = " + alpha2);
+        System.out.println("Rule 3 = " + alpha3);
+        System.out.println("Rule 4 = " + alpha4);
+        System.out.println("Rule 5 = " + alpha5);
+        System.out.println("Rule 6 = " + alpha6);
+        System.out.println("Rule 7 = " + alpha7);
+        System.out.println("Rule 8 = " + alpha8);
+        System.out.println("Rule 9 = " + alpha9);
     }
 }
